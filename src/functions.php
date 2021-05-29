b80b23a2df83df4562ef7ea4ccf977d5cfa1c695
Oranzh <oranzh.cc@gmail.com>
2021-05-29 20:01:23 +0800

    Transform a multidimensional based on parent id value array to tree structure

--- src/functions.php
+++ src/functions.php
@@ -14,13 +14,13 @@ function array_rand_value(array $array, int $numReq = 1)
 {
     if (! count($array)) return;
 
-    $keys = array_rand($array, $numReq);
+    $keyeys = array_rand($array, $numReq);
 
     if ($numReq === 1) {
-        return $array[$keys];
+        return $array[$keyeys];
     }
 
-    return array_intersect_key($array, array_flip($keys));
+    return array_intersect_key($array, array_flip($keyeys));
 }
 
 /**
@@ -33,9 +33,9 @@ function array_remove_value_recursive(array &$array, $needle)
 {
     if (! count($array)) return;
 
-    foreach ($array as $key => &$value) {
+    foreach ($array as $keyey => &$value) {
         if (is_array($value)) array_remove_value_recursive($value, $needle);
-        elseif($needle == $value) unset($array[$key]);
+        elseif($needle == $value) unset($array[$keyey]);
     }
 }
 
@@ -43,14 +43,35 @@ function array_remove_value_recursive(array &$array, $needle)
  * Remove a key from a multidimensional array
  *
  * @param array $array
- * @param $key
+ * @param $keyey
  */
 function array_remove_key_recursive(array &$array, string $needle)
 {
     if (! count($array)) return;
-    foreach ($array as $key => &$value) {
-        if ($key === $needle) unset($array[$key]);
+    foreach ($array as $keyey => &$value) {
+        if ($keyey === $needle) unset($array[$keyey]);
         elseif (is_array($value)) array_remove_key_recursive($value, $needle);
     }
 }
+
+/**
+ * Transform a multidimensional based on parent id value array to tree structure
+ *
+ * @param array   $array   based on id, parent index
+ * @param string  $parent_key  parent id index
+ * @param string  $key   id index
+ * @param string  $children   children index
+ * @return array
+ */
+function array_convert_tree($array, $parent_key = 'parent', $key = 'id', $children = 'children') {
+    $res = [];
+    foreach ($array as $value) {
+        isset($res[$value[$parent_key]]) ?: $res[$value[$parent_key]] = [];
+        isset($res[$value[$key]]) ?: $res[$value[$key]] = [];
+        $res[$value[$parent_key]][] = array_merge($value, [$children => &$res[$value[$key]]]);
+    }
+    return $res[0];
+}
+
+
 
